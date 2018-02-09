dedc31b2d716aea470d3b4f8efb448a87be8b781
zia hazara <zhazara123@gmail.com>
2018-02-09 13:59:43 +0500

    list index

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
-	validates :title, presence: true , length: {minimum: 3, maximum: 20}
+	validates :title, presence: true , length: {minimum: 3, maximum: 30}
 	validates :description, presence:true , length: {minimum:5, maximum: 50} 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # git password is javidjan
  resources :blogs
  root 'blogs#home'
  get 'blog/about', to: "blogs#about"
